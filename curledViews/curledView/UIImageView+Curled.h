2a4e3cab2ca2542514f410643cb6425ed9f26cd0
Ryan Kelly <ryank@remotevisioninc.com>
2012-02-12 20:55:25 -0600

    added category for UIImageView

---
+++ curledViews/curledView/UIImageView+Curled.h
@@ -0,0 +1,13 @@
+//
+//  UIImageView+Curled.h
+//  curledViews
+//
+//  Created by Ryan Kelly on 2/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (Curled)
+
+@end
---
+++ curledViews/curledView/UIImageView+Curled.m
@@ -0,0 +1,13 @@
+//
+//  UIImageView+Curled.m
+//  curledViews
+//
+//  Created by Ryan Kelly on 2/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UIImageView+Curled.h"
+
+@implementation UIImageView (Curled)
+
+@end
